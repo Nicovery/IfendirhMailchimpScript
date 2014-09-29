356eaf547038acafa94d6ecdea01c565f0b2741d
Nikoalas <nicolas.ludovic@gmail.com>
2014-09-29 16:34:12 +0400

    add responsive behavior

--- confirm.php
+++ confirm.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="css/main.css">
         
