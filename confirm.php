c198eb412e0a5de893d7d3ea8377432064b0122a
Nikoalas <nicolas.ludovic@gmail.com>
2015-03-23 10:13:52 +0400

    Rajout du javascript pour sliders

--- confirm.php
+++ confirm.php
@@ -77,6 +77,9 @@
     </footer>   
 <!-- Go to www.addthis.com/dashboard to customize your tools -->
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5416d2e149fb2c33"></script>
-
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
     </body>
 </html>
